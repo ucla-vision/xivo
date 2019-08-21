0bb1f74f5dd2b8c7b0b41ce1beaeedd58dfba0f0
Xiaohan Fei <hzhsfxh@gmail.com>
2019-08-21 16:57:39 -0700

    creating optimizer, linear solver and algorithm following sba_demo from g2o

--- src/optimizer.cpp
+++ src/optimizer.cpp
@@ -25,6 +25,23 @@ OptimizerPtr Optimizer::instance() {
 }
 
 Optimizer::Optimizer(const Config &cfg) {
+  if (auto solver_type{cfg.get("solver", "cholmod").asString()};
+      solver_type == "cholmod") {
+    // FIXME (xfei): what is PoseMatrixType? and what does _6_3 mean?
+    solver_ = g2o::make_unique<g2o::LinearSolverCholmod<g2o::BlockSolver_6_3::PoseMatrixType>>();
+  } else if (solver_type == "csparse") {
+    solver_ = g2o::make_unique<g2o::LinearSolverCSparse<g2o::BlockSolver_6_3::PoseMatrixType>>();
+  } else if (solver_type == "dense") {
+    solver_ = g2o::make_unique<g2o::LinearSolverDense<g2o::BlockSolver_6_3::PoseMatrixType>>();
+  }
+
+  optimizer_.setVerbose(cfg.get("verbose", true).asBool());
+
+  algorithm_ = std::make_unique<g2o::OptimizationAlgorithmLevenberg>(
+      g2o::make_unique<g2o::BlockSolver_6_3>(std::move(solver_)));
+
+  optimizer_.setAlgorithm(algorithm_.get());
+
 }
 
 } // namespace feh
