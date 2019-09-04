6fb3e49398424b38c9b5da82e8663827b83af7aa
Xiaohan Fei <hzhsfxh@gmail.com>
2019-09-03 22:20:20 -0700

    recover the evaluation script

--- misc/run_all.sh
+++ misc/run_all.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
+# Evaluation on TUM-VI room sequences.
+# Usage:
+# In project root directory, execute the following in terminal:
+# OUTPUT=/output/directory/of/your/choice misc/run_all.sh
+
 # TMPDIR=$(mktemp -d -p .)
-TUMVIROOT="/local2/Data/tumvi/exported/euroc/512_16"
+# TUMVIROOT="/local2/Data/tumvi/exported/euroc/512_16"
+TUMVIROOT="/home/feixh/Data/tumvi/exported/euroc/512_16"
 
-#python scripts/quick_run.py -out_dir $TMPDIR -root $TUMVIROOT -seq room1
-#python scripts/quick_run.py -out_dir $TMPDIR -root $TUMVIROOT -seq room2
-#python scripts/quick_run.py -out_dir $TMPDIR -root $TUMVIROOT -seq room3
-#python scripts/quick_run.py -out_dir $TMPDIR -root $TUMVIROOT -seq room4
-#python scripts/quick_run.py -out_dir $TMPDIR -root $TUMVIROOT -seq room5
-#python scripts/quick_run.py -out_dir $TMPDIR -root $TUMVIROOT -seq room6
-
-mkdir $CFG
-python scripts/run_and_eval_pyxivo.py -out_dir $CFG -root $TUMVIROOT -seq room1 ${1} &
-python scripts/run_and_eval_pyxivo.py -out_dir $CFG -root $TUMVIROOT -seq room2 ${1} &
-python scripts/run_and_eval_pyxivo.py -out_dir $CFG -root $TUMVIROOT -seq room3 ${1} &
-python scripts/run_and_eval_pyxivo.py -out_dir $CFG -root $TUMVIROOT -seq room4 ${1} &
-python scripts/run_and_eval_pyxivo.py -out_dir $CFG -root $TUMVIROOT -seq room5 ${1} &
-python scripts/run_and_eval_pyxivo.py -out_dir $CFG -root $TUMVIROOT -seq room6 ${1} &
+mkdir $OUTPUT
+python scripts/run_and_eval_pyxivo.py -out_dir $OUTPUT -root $TUMVIROOT -seq room1 ${1} &
+python scripts/run_and_eval_pyxivo.py -out_dir $OUTPUT -root $TUMVIROOT -seq room2 ${1} &
+python scripts/run_and_eval_pyxivo.py -out_dir $OUTPUT -root $TUMVIROOT -seq room3 ${1} &
+python scripts/run_and_eval_pyxivo.py -out_dir $OUTPUT -root $TUMVIROOT -seq room4 ${1} &
+python scripts/run_and_eval_pyxivo.py -out_dir $OUTPUT -root $TUMVIROOT -seq room5 ${1} &
+python scripts/run_and_eval_pyxivo.py -out_dir $OUTPUT -root $TUMVIROOT -seq room6 ${1} &
