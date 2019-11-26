ba45fc24b3814b6c9e1aef62dd59a5afa4c51416
Xiaohan Fei <hzhsfxh@gmail.com>
2019-11-25 22:27:40 -0800

    bugfix in bash script

--- misc/undistort_all.sh
+++ misc/undistort_all.sh
@@ -6,6 +6,6 @@ mkdir output
 for index in {1..6}
 do
   python scripts/pyxivo.py -use_viewer -root $TUMVIROOT -cfg cfg/tumvi_cam0.json -seq room$index -out_dir $OUTPUT -mode dump
-  python scripts/undistort.py $OUTPUT/tumvi_room$(index)_cam0 $OUTPUT/undistorted/room$index
+  python scripts/undistort.py $OUTPUT/tumvi_room${index}_cam0 $OUTPUT/undistorted/room$index
 done
 
