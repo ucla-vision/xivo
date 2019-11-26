449d69fce414d0a3670b20c22975571c2269eac5
Xiaohan Fei <hzhsfxh@gmail.com>
2019-11-25 22:23:29 -0800

    add bash script to undistort all indoor sequences

---
+++ misc/undistort_all.sh
@@ -0,0 +1,11 @@
+TUMVIROOT="/home/feixh/Data/tumvi/exported/euroc/512_16"
+OUTPUT="./output/"
+
+mkdir output
+
+for index in {1..6}
+do
+  python scripts/pyxivo.py -use_viewer -root $TUMVIROOT -cfg cfg/tumvi_cam0.json -seq room$index -out_dir $OUTPUT -mode dump
+  python scripts/undistort.py $OUTPUT/tumvi_room$(index)_cam0 $OUTPUT/undistorted/room$index
+done
+
