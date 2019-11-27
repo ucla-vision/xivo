020a5796492fefca4e88ac88ce5151dc236160cc
Stephanie Tsuei <stephanietsuei@ucla.edu>
2019-11-27 10:27:18 -0800

    outlier rejection using epipolar constraint

--- scripts/tumvi_depth_calculation.py
+++ scripts/tumvi_depth_calculation.py
@@ -1,16 +1,43 @@
 import argparse, json, os
 import numpy as np
 from scipy.spatial.transform import Rotation
+import matplotlib.pyplot as plt
 import cv2
 
+import pdb
 
 DATAROOT = "/local2/Data/tumvi_undistorted"
 TIMESTEP_WINDOW_SIZE = 10
+EPIPOLAR_TOL = 0.025
+USE_RANSAC = False
+PLOT_MATCHES = False
+
+
+def make_not_homogeneous(arr):
+    arr  = arr / arr[-1]
+    return arr[:-1]
+
+def make_homogeneous(arr):
+    return np.hstack((arr, np.array(1.0)))
+
+def skew(x):
+    return np.array([[    0, -x[2],  x[1]],
+                     [ x[2],     0, -x[0]],
+                     [-x[1],  x[0],     0]])
+
+
+def prune_matches(match_list, good_match_list):
+    new_match_list = []
+    for ind,val in enumerate(good_match_list):
+        if val==1:
+            new_match_list.append(match_list[ind])
+    return new_match_list
+
 
 class Point:
     def __init__(self, pt2d, pt4d):
         self.pt = [int(pt2d[0]), int(pt2d[1])]
-        self.pt4d = pt4d
+        self.pt3d = make_not_homogeneous(pt4d)
     def __eq__(self, other):
         return (self.pt == other.pt)
 
@@ -45,6 +72,9 @@ class MonoGTDepthCalc:
         self.K = np.array([[fx, 0, cx],
                            [0, fy, cy],
                            [0, 0, 1]])
+        self.K_inv = np.array([[1/fx,    0, -cx/fx],
+                               [   0, 1/fy, -cy/fy],
+                               [   0,    0,      1]])
 
         # For storing pts and timestamps
         #self.stored_points = [ [] for i in range(len(self.data)) ]
@@ -68,8 +98,8 @@ class MonoGTDepthCalc:
 
     def get_pose_and_filename(self, ind):
         timestamp = self.data[ind]["Timestamp"] # integer type
-        TranslationXYZ = self.data[ind]["TranslationXYZ"] # list of 3 floats
-        QuaternionWXYZ = self.data[ind]["QuaternionWXYZ"] # list of 4 floats
+        TranslationXYZ = np.array(self.data[ind]["TranslationXYZ"]) # list of 3 floats
+        QuaternionWXYZ = np.array(self.data[ind]["QuaternionWXYZ"]) # list of 4 floats
         undistort_img_filename = self.dataroot + "/undistorted/room" + \
             str(self.room_number) + "/" + str(timestamp) + ".png"
         return (timestamp, TranslationXYZ, QuaternionWXYZ, undistort_img_filename)
@@ -79,9 +109,9 @@ class MonoGTDepthCalc:
         file_name = self.pointlist_dir + "/" + str(timestamp) + ".csv"
         fp = open(file_name, "w")
         for point in self.stored_points[timestamp]:
-            fp.write("{},{},{},{},{},{}\n".format(
+            fp.write("{},{},{},{},{}\n".format(
                 point.pt[0], point.pt[1],
-                point.pt4d[0], point.pt4d[1], point.pt4d[2], point.pt4d[3]
+                point.pt3d[0], point.pt3d[1], point.pt3d[2]
             ))
         fp.close()
 
@@ -91,7 +121,40 @@ class MonoGTDepthCalc:
             self.write_file(timestamp)
 
 
-    def triangulate_pair(self, idx0, idx1):
+    def outlier_rejection(self, Q0_wxyz, Q1_wxyz, T0_xyz, T1_xyz, matches, pts0, pts1,
+        tol):
+
+        # find transformation that transforms q0 to q1
+        Q0_xyzw = [Q0_wxyz[1], Q0_wxyz[2], Q0_wxyz[3], Q0_wxyz[0]]
+        Q1_xyzw = [Q1_wxyz[1], Q1_wxyz[2], Q1_wxyz[3], Q1_wxyz[0]]
+        R_s_0 = Rotation.from_quat(Q0_xyzw)
+        R_s_1 = Rotation.from_quat(Q1_xyzw)
+        T_s_0 = T0_xyz
+        T_s_1 = T1_xyz
+
+        R_1_s = R_s_1.inv()
+        R_1_0 = R_1_s * R_s_0
+        T_1_0 = R_1_s.apply(T_s_0 - T_s_1)
+        E = skew(T_1_0).dot(R_1_0.as_dcm())
+
+        # Go through matches and mark bad ones using the essential matrix
+        good_match_list = np.ones(len(matches), dtype=np.uint8)
+        for i in range(len(matches)):
+            pt0_px = make_homogeneous(pts0[:,i])
+            pt1_px = make_homogeneous(pts1[:,i])
+
+            pt0_cam0 = self.K_inv.dot(pt0_px)
+            pt1_cam1 = self.K_inv.dot(pt1_px)
+
+            res = pt1_cam1.dot(E.dot(pt0_cam0))
+            if abs(res) > tol:
+                good_match_list[i] = 0
+
+        return good_match_list
+
+
+    def triangulate_pair(self, idx0, idx1, plot_matches, use_RANSAC, epi_tol,
+                         debug=False):
         # load two images
         (timestamp0, T0_xyz, Q0_wxyz, file0) = self.get_pose_and_filename(idx0)
         (timestamp1, T1_xyz, Q1_wxyz, file1) = self.get_pose_and_filename(idx1)
@@ -117,6 +180,7 @@ class MonoGTDepthCalc:
             return
         n_matches = len(matches)
         if n_matches == 0:
+            print("No matches on frames {}/{}".format(idx0, idx1))
             return
         points0 = np.zeros((2, n_matches))
         points1 = np.zeros((2, n_matches))
@@ -124,14 +188,42 @@ class MonoGTDepthCalc:
             points0[:,i] = kp0[match.queryIdx].pt
             points1[:,i] = kp1[match.trainIdx].pt
 
-        # Outlier rejection (uses RANSAC)
-        (_, good_match_list) = cv2.findHomography(np.transpose(points0),
-            np.transpose(points1), method=cv2.RANSAC,
-            ransacReprojThreshold=3, maxIters=2000, confidence=0.995)
-        good_match_list = np.resize(good_match_list, n_matches)
+        # plot matches before outlier rejection
+        if plot_matches:
+            img2 = cv2.drawMatches(img0, kp0, img1, kp1, matches, None,
+                flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+            plt.imshow(img2)
+            plt.show()
+
+        # Outlier rejection
+        if use_RANSAC:
+            (_, good_match_list) = cv2.findHomography(np.transpose(points0),
+                np.transpose(points1), method=cv2.RANSAC,
+                ransacReprojThreshold=3, maxIters=2000, confidence=0.995)
+            good_match_list = np.resize(good_match_list, n_matches)
+            matches = prune_matches(matches, good_match_list)
+        else:
+            good_match_list = self.outlier_rejection(Q0_wxyz, Q1_wxyz, T0_xyz, T1_xyz,
+                matches, points0, points1, epi_tol)
+            matches = prune_matches(matches, good_match_list)
+
+        # plot matches after outlier rejection
+        if plot_matches:
+            img3 = cv2.drawMatches(img0, kp0, img1, kp1, matches, None,
+                flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+            plt.imshow(img3)
+            plt.show()
+
+
+        n_good_matches = np.count_nonzero(good_match_list)
+        if n_good_matches==0:
+            print("No good matches on frames {}/{}".format(idx0, idx1))
+            return
         points0 = points0[:,good_match_list==1]
         points1 = points1[:,good_match_list==1]
-        n_good_matches = np.count_nonzero(good_match_list)
+
+        if debug:
+            pdb.set_trace()
 
         # Triangulate matches
         try:
@@ -157,18 +249,20 @@ class MonoGTDepthCalc:
             if pt_obj not in self.stored_points[timestamp1]:
                 self.stored_points[timestamp1].append(pt_obj)
         
-        # display to screen
+        # Print results
         print("Frame {}/{}: {}/{} features, {} matches, {} good matches".format(
             idx0, idx1, len(kp0), len(kp1), n_matches, n_good_matches
         ))
 
 
 
-    def triangulate_all_pairs(self, timestep_window_size):
+    def triangulate_all_pairs(self, timestep_window_size, use_RANSAC=False,
+        epi_tol=1e-3, plot_matches=False):
         num_pairs = len(self.data) - timestep_window_size
 
         for i in range(num_pairs):
-            self.triangulate_pair(i,i+timestep_window_size)
+            self.triangulate_pair(i,i+timestep_window_size, use_RANSAC=use_RANSAC,
+                                  epi_tol=epi_tol, plot_matches=plot_matches)
 
 
 
@@ -180,6 +274,7 @@ if __name__ == "__main__":
         for i in range(1,6):
             print("ROOM {}".format(i))
             mdc = MonoGTDepthCalc(DATAROOT, i, feature_type)
-            mdc.triangulate_all_pairs(TIMESTEP_WINDOW_SIZE)
+            mdc.triangulate_all_pairs(TIMESTEP_WINDOW_SIZE, use_RANSAC=USE_RANSAC,
+                                      epi_tol=EPIPOLAR_TOL, plot_matches=PLOT_MATCHES)
             mdc.write_all_files()
 
