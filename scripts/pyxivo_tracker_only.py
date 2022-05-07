6afb83b070df6fbdabe92a8fc1bcfa242179432d
Parth Agrawal <agrawal.parth10@gmail.com>
2022-05-07 02:16:03 -0700

    Added python interface

--- scripts/pyxivo_tracker_only.py
+++ scripts/pyxivo_tracker_only.py
@@ -7,7 +7,6 @@ import pyxivo
 import savers
 
 
-
 parser = argparse.ArgumentParser()
 parser.add_argument("-root", default="/media/data1/Data/tumvi/exported/euroc/512_16",
     help="location of VIO dataset")
@@ -78,15 +77,10 @@ def main(args):
     if args.dataset == 'tumvi':
         img_dir = os.path.join(args.root, 'dataset-{}_512_16'.format(args.seq),
                                'mav0', 'cam{}'.format(args.cam_id), 'data')
-
-        imu_path = os.path.join(args.root, 'dataset-{}_512_16'.format(args.seq),
-                                'mav0', 'imu0', 'data.csv')
     elif args.dataset == 'cosyvio':
         img_dir = os.path.join(args.root, 'data', args.sen, args.seq, 'frames')
-        imu_path = os.path.join(args.root, 'data', args.sen, args.seq, 'data.csv')
     elif args.dataset in ['xivo', 'carla']:
         img_dir = os.path.join(args.root, args.seq, 'cam0', 'data')
-        imu_path = os.path.join(args.root, args.seq, 'imu0', 'data.csv')
     else:
         raise ValueError('unknown dataset argument; choose from tumvi, xivo, cosyvio, carla')
 
@@ -105,15 +99,6 @@ def main(args):
                     ts = int(larr[0])
                     png_file = os.path.join(img_dir, larr[1])
                     data.append((ts,png_file))
-
-    with open(imu_path, 'r') as fid:
-        for l in fid.readlines():
-            if l[0].isdigit():
-                v = l.strip().split(',')
-                ts = int(v[0])
-                w = [float(x) for x in v[1:4]]
-                t = [float(x) for x in v[4:]]
-                data.append((ts, (w, t)))
 
     data.sort(key=lambda tup: tup[0])
 
@@ -144,17 +129,10 @@ def main(args):
         for i, (ts, content) in enumerate(data):
             if i > 0 and i % 1000 == 0:
                 print('{:6}/{:6}'.format(i, len(data)))
-            if isinstance(content, tuple):
-                gyro, accel = content
-                estimator.InertialMeas(ts, gyro[0], gyro[1], gyro[2], accel[0],
-                                    accel[1], accel[2])
-            else:
-                estimator.VisualMeas(ts, content)
-                if estimator.UsingLoopClosure():
-                    estimator.CloseLoop()
-                estimator.Visualize()
-                if args.mode != 'runOnly':
-                    saver.onVisionUpdate(estimator, datum=(ts, content))
+            estimator.VisualMeasTrackerOnly(ts, content)
+            estimator.Visualize()
+            if args.mode != 'runOnly':
+                saver.onVisionUpdate(estimator, datum=(ts, content))
 
     finally:
         if args.mode != 'runOnly':
